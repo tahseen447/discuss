2e0977a6eedc51d630106ac8fc09edd6f4311d2e
Tahseen Siddiqui <tsiddiqui@cogility.com>
2019-07-17 14:59:07 -0700

    add error tag to new form add index route write out index def

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -2,14 +2,24 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
   end
 
   def create(conn, %{"topic" => topic} = params) do
-    #changeset = Topic.changeset(%Topic{}, topic)
-    #render conn, "index.html"
-    #IO.inspect params
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, topic} -> render conn, "index.html"
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
+
+
+
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -16,7 +16,6 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
     get "/topics", TopicController, :index
