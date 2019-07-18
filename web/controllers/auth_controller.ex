09d074e6776b2802a883f3514bf485896f2afd6e
Tahseen Siddiqui <tsiddiqui@cogility.com>
2019-07-18 13:22:11 -0700

    sign out

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -11,6 +11,14 @@ defmodule Discuss.AuthController do
     signin(conn, changeset)
   end
 
+  def signout(conn, _params) do
+    IO.puts("Im here")
+    conn
+    |> configure_session(drop: true)
+    |> redirect(to: topic_path(conn, :index))
+  end
+
+
   defp signin(conn, changeset) do
     case insert_or_update_user(changeset) do
       {:ok, user} ->
--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Discuss.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
   end
 
   pipeline :api do
@@ -29,8 +30,10 @@ defmodule Discuss.Router do
   scope "/auth", Discuss do
     pipe_through :browser
 
+    get "/signout", AuthController, :signout
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
+
   end
   # Other scopes may use custom stacks.
   # scope "/api", Discuss do
